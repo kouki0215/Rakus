038d3ade1212ddd8cde7efcfc51e2b237879d81f
hishiko <97656756+kouki0215@users.noreply.github.com>
2022-11-23 22:13:25 +0900

    Delete public directory

--- public/javascripts/chat_header.js
+++
@@ -1,9 +0,0 @@
-const position = $('#position').val();
-var textForm = document.getElementById("chat-header");
-
-if(position == '上司'){
-  textForm.innerHTML = '田中さん（部下）';
-}
-else{
-  textForm.innerHTML = '佐藤さん（上司）';
-}
