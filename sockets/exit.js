20bc5b6145cf0e729d44df0cd77f840864c33b65
hishiko <97656756+kouki0215@users.noreply.github.com>
2022-11-23 22:13:08 +0900

    Delete sockets directory

--- sockets/exit.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function (socket) {
-    // 退室メッセージをクライアントに送信する
-    socket.on('sendExitEvent', function(userName, position){
-        socket.broadcast.emit('receiveExitEvent', userName + 'さん(' + position + ')が退出しました。');
-    });
-};
